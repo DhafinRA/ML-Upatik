a034cfb6c03dd4be688ff114389cab5370ff54fd
DhafinRA <dhafinrizkyaulia@gmail.com>
2024-05-15 11:42:09 +0700

    adding mlapi.py

--- Deployment/mlapi.py
+++ Deployment/mlapi.py
@@ -5,11 +5,20 @@ import PIL.ImageOps
 import io
 import pickle
 import os
+import pandas as pd
 os.system("cls")
 app = FastAPI()
 
-with open('model.pkl','rb') as f:
+with open('foodXception.pkl','rb') as f:
     model = pickle.load(f)
+
+def getCalorie():
+    nutrisi_csv = pd.read_csv('nutrisi_origin.csv')
+    Kalori = nutrisi_csv['Kalori']
+    Protein = nutrisi_csv['Protein']
+    Lemak = nutrisi_csv['Lemak']
+
+    return Kalori,Protein,Lemak
 
 @app.get('/')
 async def hello():
@@ -19,13 +28,21 @@ async def hello():
 async def predict(file: UploadFile = File(...)):
     img = await file.read()
     img = PIL.Image.open(io.BytesIO(img)).convert('RGB')
-    img = img.resize(size=(224,224))
+    img = img.resize(size=(244,244))
     img = np.array(img)
     img = np.expand_dims(img,axis=0)
     preds = model.predict(img)
-    
-    # return {"Prediction" : int(preds[0])}
-    return preds
 
-hasil = predict()
-print(hasil)
+    Kalori,Protein,Lemak = getCalorie()
+  
+    # return f"Prediction : {np.argmax(preds[0])}"
+    return {"Prediction" : int(np.argmax(preds[0])),
+            "Kalori" : int(Kalori),
+            "Protein" : Protein,
+            "Lemak" : Lemak}
+# img = PIL.Image.open(io.BytesIO(img)).convert('RGB')
+# img = img.resize(size=(224,224))
+# img = np.array(img)
+# img = np.expand_dims(img,axis=0)
+# preds = model.predict(img)
+# print(preds)
